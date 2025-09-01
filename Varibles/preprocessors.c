229309ca61c1920f345252ae1def86ad1d611e77
code-saahe <165862365+code-saahe@users.noreply.github.com>
2025-09-01 01:03:08 -0400

    worked on more macros

--- Varibles/preprocessors.c
+++ Varibles/preprocessors.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #define PI 3.14f
-#undef PI
 #define DEBUG
 
 int main()
 {
 #ifdef DEBUG
+#warning "peformance will be reduced"
     printf("we are in debugging mode\n");
+#endif
+#ifdef PI
+    printf("%f", PI);
 #endif
 
     return 0;
