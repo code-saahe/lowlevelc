44dbb22ad8b9bf422408b42146b735a7d88f31a9
code-saahe <165862365+code-saahe@users.noreply.github.com>
2025-08-31 21:56:15 -0400

    did varible scopes

--- Varibles/variables.c
+++ Varibles/variables.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
+int g_NumPersons = 0;
+
 int main()
 {
     int personID = 0;
     int person2ID = personID + 1;
+    g_NumPersons = person2ID + 50;
     {
-        printf("hello world\n%d\n%d", personID, person2ID);
+        printf("hello world\n%d\n%d\n%d", personID, person2ID, g_NumPersons);
     }
     return 0;
 }
