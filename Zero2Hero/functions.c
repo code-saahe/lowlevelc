e679870ddf2d1693e4ed1fed18457eac1b557313
code-saahe <165862365+code-saahe@users.noreply.github.com>
2025-09-02 18:39:38 -0400

    just finished up with functions

--- Zero2Hero/functions.c
+++ Zero2Hero/functions.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+#define Money int
+int multiply_data(int x, int y)
+{
+    Money z = x * y;
+    return z;
+}
 int main(int argc, char *argv[])
 {
-    printf("%s", argc);
+    Money multi = multiply_data(5, 5);
+    printf("%d\n", multi);
     return 0;
 }
